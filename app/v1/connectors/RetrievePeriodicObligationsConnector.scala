fa729b4f64b1c7aff7c88649d706fc4e43ab987b
“Thepworth95” <56401451+Thepworth95@users.noreply.github.com>
2020-04-16 10:57:53 +0100

    make Retrieve periodic obligations connector

---
+++ app/v1/connectors/RetrievePeriodicObligationsConnector.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import config.AppConfig
+import javax.inject.Inject
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import v1.connectors.httpparsers.StandardDesHttpParser._
+import v1.models.request.retrievePeriodObligations.RetrievePeriodicObligationsRequest
+import v1.models.response.retrievePeriodObligations.RetrievePeriodObligationsResponse
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RetrievePeriodicObligationsConnector @Inject()(val http: HttpClient,
+                                                     val appConfig: AppConfig) extends BaseDesConnector {
+  def retrievePeriodicObligations(request: RetrievePeriodicObligationsRequest)
+                                        (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[DesOutcome[RetrievePeriodObligationsResponse]] = {
+
+    val queryParams: String = Seq(
+      "from" -> request.fromDate,
+      "to" -> request.toDate,
+      "status" -> request.status
+    ).collect {
+      case (k, Some(v)) => s"$k=$v"
+    }.mkString("&")
+
+    val url = s"enterprise/obligation-data/nino/${request.nino}/ITSA?$queryParams"
+
+    get(
+      DesUri[RetrievePeriodObligationsResponse](s"$url")
+    )
+  }
+}
---
+++ test/v1/connectors/RetrievePeriodicObligationsConnectorSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import mocks.MockAppConfig
+import uk.gov.hmrc.domain.Nino
+import v1.mocks.MockHttpClient
+import v1.models.domain.business.MtdBusiness
+import v1.models.domain.status.DesStatus
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.retrievePeriodObligations.RetrievePeriodicObligationsRequest
+import v1.models.response.retrievePeriodObligations.RetrievePeriodObligationsResponse
+
+import scala.concurrent.Future
+
+class RetrievePeriodicObligationsConnectorSpec extends ConnectorSpec {
+
+  private val validNino = Nino("AA123456A")
+  private val validtypeOfBusiness = MtdBusiness.`foreign-property`
+  private val validincomeSourceId = "XAIS123456789012"
+  private val validfromDate = "2018-04-06"
+  private val validtoDate = "2019-04-05"
+  private val validStatus = DesStatus.O
+
+  class Test extends MockHttpClient with MockAppConfig {
+    val connector: RetrievePeriodicObligationsConnector = new RetrievePeriodicObligationsConnector(
+      http = mockHttpClient, appConfig = mockAppConfig)
+    val desRequestHeaders: Seq[(String, String)] = Seq("Environment" -> "des-environment", "Authorization" -> s"Bearer des-token")
+    MockedAppConfig.desBaseUrl returns baseUrl
+    MockedAppConfig.desToken returns "des-token"
+    MockedAppConfig.desEnvironment returns "des-environment"
+  }
+
+  "retrieve" should {
+    "return a result" when {
+      "the downstream call is successful and a source is passed in" in new Test {
+        val request = RetrievePeriodicObligationsRequest(validNino,
+          Some(validtypeOfBusiness),
+          Some(validincomeSourceId),
+          Some(validfromDate),
+          Some(validtoDate),
+          Some(validStatus))
+        val outcome = Right(ResponseWrapper(correlationId, RetrievePeriodObligationsResponse(Seq())))
+
+        MockedHttpClient.
+          get(
+            url = s"$baseUrl/enterprise/obligation-data/nino/${request.nino}/ITSA?from=${request.fromDate}&to=${request.toDate}&status=${request.status}",
+            requiredHeaders = "Environment" -> "des-environment", "Authorization" -> s"Bearer des-token"
+          ).returns(Future.successful(outcome))
+        await(connector.retrievePeriodicObligations(request)) shouldBe outcome
+      }
+    }
+  }
+}
