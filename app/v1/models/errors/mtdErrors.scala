d0346ab8f03e20bd526c1e687f41d1be7d6f20b4
“Thepworth95” <56401451+Thepworth95@users.noreply.github.com>
2020-04-20 15:25:14 +0100

    amend not found error

--- app/v1/models/errors/mtdErrors.scala
+++ app/v1/models/errors/mtdErrors.scala
@@ -31,10 +31,10 @@ object FromDateFormatError extends MtdError("FORMAT_FROM_DATE", "The provided fr
 object ToDateFormatError extends MtdError("FORMAT_TO_DATE", "The provided toDate is invalid")
 object StatusFormatError extends MtdError("FORMAT_STATUS", "The provided status is invalid")
 object TypeOfBusinessFormatError extends MtdError("FORMAT_TYPE_OF_BUSINESS", "The provided type of business is invalid")
-object FromDateFormatError extends MtdError("FORMAT_FROM_DATE", "The provided fromDate is invalid")
-object ToDateFormatError extends MtdError("FORMAT_TO_DATE", "The provided toDate is invalid")
 
 object MissingTypeOfBusinessError extends MtdError("MISSING_TYPE_OF_BUSINESS", "The type of business query parameter must be provided when an incomeSourceId is supplied.")
+
+object NoObligationsFoundError extends MtdError("NO_OBLIGATIONS_FOUND", "No obligations found using this filter")
 // Rule Errors
 object RuleTaxYearNotSupportedError
     extends MtdError("RULE_TAX_YEAR_NOT_SUPPORTED", "Tax year not supported, because it precedes the earliest allowable tax year")
