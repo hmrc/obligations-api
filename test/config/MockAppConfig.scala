f58376b6548736042c2215c9c613e4cabd74cd14
Michael Olafisoye <molafisoye@gmail.com>
2024-09-05 23:24:20 +0100

    MTDSA-26341 remove scope api definition factory

--- test/config/MockAppConfig.scala
+++ test/config/MockAppConfig.scala
@@ -48,9 +48,6 @@ trait MockAppConfig extends MockFactory {
     def confidenceLevelConfig: CallHandler0[ConfidenceLevelConfig] =
       (() => mockAppConfig.confidenceLevelConfig).expects()
 
-    def confidenceLevelCheckEnabled: CallHandler[ConfidenceLevelConfig] =
-      (() => mockAppConfig.confidenceLevelConfig).expects()
-
     def endpointAllowsSupportingAgents(endpointName: String): CallHandler[Boolean] =
       (mockAppConfig.endpointAllowsSupportingAgents(_: String)).expects(endpointName)
 
