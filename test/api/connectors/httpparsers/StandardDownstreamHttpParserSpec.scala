da14a892fba60b70782df7cfd716803b9667e21d
Behrouz Karimi <125777824+bkarimii@users.noreply.github.com>
2026-06-09 14:35:12 +0100

    MTDSA-34731 (#186)

--- test/api/connectors/httpparsers/StandardDownstreamHttpParserSpec.scala
+++ test/api/connectors/httpparsers/StandardDownstreamHttpParserSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -223,7 +223,7 @@ class StandardDownstreamHttpParserSpec extends UnitSpec {
                                      |}
       """.stripMargin)
 
-    s"receiving a response with a bvr errors" should {
+    "receiving a response with a bvr errors" should {
       "return an outbound BUSINESS_ERROR error containing the BVR ids" in {
         val httpResponse = HttpResponse(BAD_REQUEST, singleBvrJson, Map("CorrelationId" -> Seq(correlationId)))
 
